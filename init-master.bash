0680d1e98857baaba6868335a0d83cc8f650b8c3
Mike Lambert <lambert8@illinois.edu>
2021-03-04 16:46:08 -0600

    Update stale flannel URL in docs

--- init-master.bash
+++ init-master.bash
@@ -17,7 +17,7 @@ kubeadm init --pod-network-cidr=${POD_NETWORK_CIDR} && \
 
 if [ "$POD_NETWORK" == "flannel" ]; then
 	# Install flannel
-	kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
+	kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 elif [ "$POD_NETWORK" == "weave" ]; then
 	# Install weave
 	# From https://kubernetes.io/docs/setup/independent/create-cluster-kubeadm/
